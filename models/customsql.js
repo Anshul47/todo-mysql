ebaa70be8f067d135e9679eb20a20b6c98c41e6e
Anshul Reejonia <anshul.reejonia@gmail.com>
2018-12-24 17:02:49 +0530

    Add px to table

--- models/customsql.js
+++ models/customsql.js
@@ -1,9 +1,9 @@
 
 const mysql = require('../db/dbcon.js');
 
-var  sql1 = "CREATE TABLE toto ( id varchar(36) NOT NULL, uid varchar(36) NOT NULL, todo_text text NOT NULL, comp_flag tinyint(1) NOT NULL, comp_date date NOT NULL, cdate datetime NOT NULL, udate datetime NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf32;";
+var  sql1 = "ALTER TABLE toto ADD PRIMARY KEY (id);";
 
-var  sql2 = "CREATE TABLE user (uid varchar(36) NOT NULL, name varchar(255) NOT NULL, email varchar(355) NOT NULL, password text NOT NULL, token text NOT NULL, cdate datetime NOT NULL, udate datetime NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=utf32;";
+var  sql2 = "ALTER TABLE user ADD PRIMARY KEY (uid);";
 
 
 var runSqlQuery1 = function (){
